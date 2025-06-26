ad62c7d4ead5f47c0721bb5053944b0558740481
Signed-B <beckett@becketthyde.com>
2025-06-26 16:05:45 -0600

    working glass

---
+++ react-measures/src/components/FrostedGlass.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './FrostedGlass.module.css';
+
+const FrostedGlass = () => {
+  return (
+    <div className={styles.frostedGlass}>
+      <div className={styles.frostedGlassBackdrop}></div>
+    </div>
+  );
+};
+
+export default FrostedGlass; 
